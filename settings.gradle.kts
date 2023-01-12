761f614091d26936a56ca418b22232bd16e37882
JisuNa <kuu2002@naver.com>
2023-01-12 15:07:25 +0900

    Add modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,8 @@ rootProject.name = "starlucks"
 include("starlucks-main")
 include("starlucks-common")
 include("starlucks-store")
+include("starlucks-order")
+include("starlucks-member")
+include("starlucks-payment")
+include("starlucks-order-processing")
+include("starlucks-notification")
